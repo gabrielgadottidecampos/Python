f561536d804e63301f24e06b44a97884711a5745
Gabriel <gabriel-gadotti@hotmail.com>
2021-03-10 22:24:24 -0300

    Jogo de Adivinhação Versão 1.0

--- Jogo da Forca/forca.py
+++ Jogo da Forca/forca.py
@@ -1,26 +1,147 @@
+import random
+
 def jogar():
+    carrega_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while (not enforcou and not acertou):
+
+        chute = pede_chute()
+        if (chute in palavra_secreta):
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            erros += 1
+            desenha_forca(erros)
+        enforcou = erros == 7
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+    if (acertou):
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor(palavra_secreta)
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+def carrega_abertura():
     print("**********************************")
     print("Bem Vindo ao Jogo da Forcar!")
     print("**********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
-    enforcou = False
-    acertou = False
-    print(letras_acertadas)
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
 
-    while(not enforcou and not acertou):
-        chute = input("Digite uma letra: ")
-        chute = chute.strip() # tira os espaços da palavra
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()): # deixa a palavra digitada e a palavra_secreta em maiusculo, deixando a comparação sempre correta
-                letras_acertadas[index] = letra
-            index = index + 1
-        print(letras_acertadas)
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
 
+def pede_chute():
+    chute = input("Digite uma letra: ")
+    chute = chute.strip().upper()  # tira os espaços da palavra
+    return chute
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (
+            chute.upper() == letra.upper()):  # deixa a palavra digitada e a palavra_secreta em maiusculo, deixando a comparação sempre correta
+            letras_acertadas[index] = letra
+        index += 1
 
-if(__name__ == "__main__"):
+if (__name__ == "__main__"):
     jogar()
 
 #################################################
@@ -28,5 +149,10 @@ if(__name__ == "__main__"):
 strip() -> tira os espaço da palavra 
 upper() -> trasforma todas as letras em maiuscula 
 capitalize() -> Palavra inicia em maiusculo  
+lista = [] -> lista onde pode ocorrer repetição
+append() -> adiciona algo a lista. OBS só funciona na lista []
+lista = {} -> lista onde não pode ocorrer repetição e não contem um indice então não sabemos em qual ordem vem os valores 
+add() - > adiciona elementos em uma lista. OBS só funciona na lista {}
+
 
 """
--- Jogo da Forca/rascunho.py
+++ Jogo da Forca/rascunho.py
@@ -1,2 +1,27 @@
-valores = ("a","b","c","d","e")
-print(len(valores))
+"""lista = {123, 456, 789}
+print(lista)
+print(lista)
+lista.add(987)
+print(lista)
+for list in lista:
+    print(list)
+instrutores = ["Gabriel", 39 , "Tais", 20, "Marcio", 40]
+print(instrutores)
+teste = ("Gabriel", 39)
+teste1 = ("Tais", 20)
+teste2 = ("Marcio", 40)
+resultado = [teste, teste1, teste2]
+print(resultado)
+print(resultado[1][1])
+p = ["w", "k", "d"]
+res = ["w"] not in p
+print(res)
+frutas = ["maçã", "banana", "laranja", "melancia"]
+lista = [fruta.upper() for fruta in frutas]
+print(lista)
+arquivo = open("palavras.txt", "a")
+arquivo.write("Banana")
+"""
+arquivo = open("palavras.txt", "r")
+for linha in arquivo:
+    print(linha)
