f5e90504f62e279e7127f9517b1481559fb77797
Gabriel <gabriel-gadotti@hotmail.com>
2021-03-13 21:37:57 -0300

    conta_banco 1.2

--- conta banco/conta.py
+++ conta banco/conta.py
@@ -1,6 +1,41 @@
 class Conta:
     def __init__(self, numero, nome, saldo, limite):
-        self.numero = numero
-        self.titular = nome
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = nome
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_saque = self.__saldo + self.__limite
+        return valor_a_sacar <=  valor_disponivel_saque
+
+    def saque(self, valor):
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O Valor {} é maior que o limite disponivel".format(valor))
+
+    def transfere(self, valor, destino):
+        self.saque(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+    @property
+    def limite(self):
+        return self.limite
+    @limite.setter
+    def set_limite(self, limite):
+        self.__limite = limite
+    @staticmethod
+    def codigo_banco():
+        return "001"
--- conta banco/conta_banco.py
+++ conta banco/conta_banco.py
@@ -1,22 +1,5 @@
-def conta_banco(numero, nome, saldo, limite):
-    conta = {"Numero":numero, "nome":nome, "saldo":saldo, "limite":limite}
-    return conta
+from datas import Retangulo
 
-def depositar(conta, valor):
-    conta ["saldo"] += valor
-
-def sacar(conta, valor):
-    conta["saldo"] -= valor
-
-def extrato(conta):
-    print("Saldo {}".format(conta["saldo"]))
-
-
-conta = conta_banco(123, "gadotti", 100.0, 500.0)
-print("saldo na conta {} ".format(conta["saldo"]))
-valor = int(input("Digite um valor de deposito: "))
-depositar(conta, valor)
-print("saldo na conta {} ".format(conta["saldo"]))
-valor = valor = int(input("Digite um valor de saque: "))
-sacar(conta, valor)
-print("saldo na conta {} ".format(conta["saldo"]))
+r = Retangulo(7, 6)
+r.area = 7
+print(r.obter_area())
